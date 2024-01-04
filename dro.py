Fixed dro.py issue, was not MOSEK after all but sign mistake. Yet MOSEK says infeasible if problem is unbounded.

dro.py
@@ -51,11 +51,7 @@ def drinc_cost(support: Polytope, radius: float):
         _psi = cp.Variable((_H.shape[0], _n))
 
         # Equality constraints
-        cons = [mean_si >= cp.sum(_s) / _n]
-
-        print((cp.sum(_s) / _n))
-        print(_s.shape)
-        print(mean_si)
+        cons = [mean_si == cp.sum(_s) / _n]
 
         # Inequality constraints
         cons += [_l >= 0, _mu >= 0, _psi >= -_mu, _a >= 0]
@@ -65,7 +61,7 @@ def drinc_cost(support: Polytope, radius: float):
             + cp.kron(np.diag([-1, 1]), 4 * q)
         for i, (xii, (mui, psii)) in enumerate(zip(xis.T, zip(_mu.T, _psi.T))):
             # Element [1,1]
-            _scalar = _s[[i]] - _h.T @ psii + _l * xii.T @ xii
+            _scalar = _s[[i]] - _h.T @ (psii + mui*2) + _l * xii.T @ xii
             # Elements [2:end, 1]
             _vec = cp.hstack([_l * xii * 2 + _H.T @ psii, _H.T @ mui])[:, None]
 
@@ -74,7 +70,6 @@ def drinc_cost(support: Polytope, radius: float):
 
             # Orthogonality constraint
             _h_mui = _H.T @ mui[:, None]
-            cons += [mui == 0]  # TODO: Remove this and get why mosek fucks up
             cons += [cp.bmat([[_a, _h_mui.T],
                               [_h_mui, np.eye(q.shape[0]) * _l - q*0]]) >> 0]
 

tests/test_dro.py
@@ -14,7 +14,8 @@ def test_drinc_cost(verbose=False):
     # Parameters
     _d = 2
     _q = np.eye(_d)
-    radius = 2.0
+    radius = 0.25
+    sq2 = np.sqrt(2.0)
 
     # Unit box support set
     support = Polytope()
@@ -24,12 +25,13 @@ def test_drinc_cost(verbose=False):
 
     # Center distribution with two samples
     # One at zero and one at d=1 from border of support
-    xis_train = np.array([[1.0-0.707, 0], [1.0-0.707, 0]])
+    xis_train = np.array([[1.0-1/sq2/2, 0], [1.0-1/sq2/2, 0]])
 
-    # Worst case distribution and risk: move by 1 + sqrt(2)/sqrt(2) = 2
-    xis_test = np.array([[1.0, 0.707], [1.0, 0.707]])
+    # Worst case distribution and risk: each sample moves by
+    # sqrt(2)/sqrt(2)/2 = 0.5 so the Wasserstein distance is 0.5^2
+    xis_test = np.array([[1.0, 1/sq2/2], [1.0, 1/sq2/2]])
     wc_risk = np.mean([xis_test[:, i].T @ _q @ xis_test[:, i]
-                       for i in range(_d)])
+                       for i in range(xis_test.shape[1])])
 
     # get the cost and optimize
     cost, cons = drinc_cost(support, radius)

tests/test_simulate.py
@@ -16,11 +16,16 @@ def test_simulate(verbose=False):
     x, u, y = simulate(np.kron(np.arange(6), np.ones((3, 1))), sys, xi)
 
     if verbose:
-        print(x, u, y)
+        print("states:")
+        print(x)
+        print("inputs:")
+        print(u)
+        print("outputs:")
+        print(y)
 
-    assert (x == np.vstack((np.zeros((4, 1)), 15*np.ones((8, 1))))).all()
-    assert (u == np.vstack((np.zeros((2, 1)), 15*np.ones((4, 1))))).all()
-    assert (y == np.vstack((np.zeros((2, 1)), 30*np.ones((4, 1))))).all()
+    assert np.all(x == np.vstack((np.zeros((4, 1)), 15*np.ones((8, 1)))))
+    assert np.all(u == np.vstack((np.zeros((2, 1)), 15*np.ones((4, 1)))))
+    assert np.all(y == np.vstack((np.zeros((2, 1)), 30*np.ones((4, 1)))))
 
     if verbose:
         print("All tests passed")