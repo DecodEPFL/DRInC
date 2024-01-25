fixed stupid sign mistake again.

dro.py
@@ -54,16 +54,16 @@ def drinc_cost(support: Polytope, radius: float):
         cons = [mean_si == cp.sum(_s) / _n]
 
         # Inequality constraints
-        cons += [_l >= 0, _mu >= 0, _psi >= -_mu, _a >= 0]
+        cons += [_l >= 0, _mu >= 0, _psi >= _mu, _a >= 0]
 
         # Matrix inequality constraints
         _q2 = cp.kron(np.diag([1, 0]), 4 * np.eye(q.shape[0]) * _l) \
             + cp.kron(np.diag([-1, 1]), 4 * q)
         for i, (xii, (mui, psii)) in enumerate(zip(xis.T, zip(_mu.T, _psi.T))):
             # Element [1,1]
-            _scalar = _s[[i]] - _h.T @ (psii + mui*2) + _l * xii.T @ xii
+            _scalar = _s[[i]] - _h.T @ psii + _l * xii.T @ xii
             # Elements [2:end, 1]
-            _vec = cp.hstack([_l * xii * 2 + _H.T @ psii, _H.T @ mui])[:, None]
+            _vec = cp.hstack([_l * xii * 2 - _H.T @ psii, _H.T @ mui])[:, None]
 
             # Main LMI
             cons += [cp.bmat([[_scalar, _vec.T], [_vec, _q2]]) >> 0]

experiments/double_integrator.py
@@ -35,21 +35,22 @@ def double_integrator_experiment(radius=0.1, verbose=False):
     # Time horizons, problem ill conditionned if t_fir < 5
     t_fir, t_test = 6, 40
     # Feasible set size, cvar probability level, and noise level
-    feas_r, p_level, noise = 70.0, 5e-2, 1.0  # 70
+    feas_r, p_level, noise = 60.0, 5e-2, 1.0  # 70
     # Number of samples. The list contains parameters for distributions.
     # Their values are explained in utils/distributions.py
-    _ptrain, _ptest = (25, [0.99, 0.99]), (100, [1.01, 1.01])
+    _ptrain, _ptest = (25, [1.0, 0.95]), (100, [1.0, 1.05])
 
     # System definition
     sys = LinearSystem()
     sys.a, sys.b, sys.c = np.array([[1, 1], [0, 1]]), \
         np.array([[0], [1]]), np.array([[1, 0]])
 
-    # Support definition
+    # Support definition as a box [-0.4*noise, 1.2*noise]^d
     support = Polytope()
     support.h = np.vstack((np.eye((_n + _p) * t_fir),
                            -np.eye((_n + _p) * t_fir)))
-    support.g = noise * np.ones((2 * (_n + _p) * t_fir, 1))
+    support.g = noise * np.array(([1.2] * (_n + _p) * t_fir)
+                                 + ([0.4] * (_n + _p) * t_fir))[:, None]
 
     # Feasible set definition
     fset = Polytope()

tests/test_dro.py
@@ -17,9 +17,9 @@ def test_drinc_cost(verbose=False):
 
     # Unit box support set
     support = Polytope()
-    support.h = np.vstack((np.eye(_d),
-                           -np.eye(_d)))
-    support.g = np.ones((2 * _d, 1))
+    support.h = np.vstack((1.0*np.eye(_d),
+                           -1.0*np.eye(_d)))
+    support.g = np.array([1.0] * _d + [0.0] * _d)[:, None]
 
     # Center distribution with two samples
     # One at zero and one at d=1 from border of support

utils/setup_controllers.py
@@ -50,7 +50,7 @@ def get_controllers(t_fir: int, radius: float, p_level: float,
 
     # Obtain robust closure, cut low probability half of support for feasibility
     rob_support = support
-    rob_support.g = support.g.copy()/2
+    rob_support.g = support.g.copy() - 0.4*np.sign(support.g)
     rob = synthesize_robust(sys, t_fir, fset, rob_support, verbose)
 
     # Make lqg closure for compatibility. Use empirical covariances